511ec64898c158d3ca354cb5d2525888f967d220
2300090230 <2300090230@kluniversity.in>
2025-01-07 07:37:05 +0530

    second commit

---
+++ p1.js
@@ -0,0 +1,20 @@
+const product={
+    name:'Parker',
+    rating:3.4,
+    offer:"5%",
+    price:270,
+    isDeal:true
+
+
+
+}
+const profile={
+    name:"sharadha kapra",
+    following:4,
+    followers:54000,
+    posts:195,
+    role:"Entrepenur",
+
+}
+console.log(typeof profile["posts"])
+console.log(product)
