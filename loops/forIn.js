09841e53ebdb625c061d66d5516138ab43a4961a
2300090230 <2300090230@kluniversity.in>
2025-01-12 11:36:28 +0530

    loops practiced

---
+++ loops/forIn.js
@@ -0,0 +1,11 @@
+let student ={
+    name:"Laxman",
+    id:2300090230,
+    cgpa:9.8,
+    isPass:true
+
+
+}
+for(let i in student){
+    console.log("key=",i,"value=",student[i])
+}
---
+++ loops/forOf.js
@@ -0,0 +1,4 @@
+let str='LaxmanCollege';
+for(i of str){
+    console.log("i=",i)
+}
